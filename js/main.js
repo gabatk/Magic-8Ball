d2e714063d2354f4ae357eaae3d57ff9e00c674f
gabatk <gabriela.trybuszek@gmail.com>
2023-01-05 21:03:45 +0100

    JavaScript functions in progress.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 const ball = document.querySelector('.billard-ball');
 const ballImg = document.querySelector('.ball-img');
+const input = document.querySelector('.question-input');
+const errorField = document.querySelector('.error');
+const answerField = document.querySelector('.answer');
+
+const ballAnswer = [
+	'Yes.',
+	'No.',
+	'Better ask me another question...',
+	'Definitely no',
+	'Probably yes',
+	'Ask again later',
+	'I am not able to answer now',
+	'My sources say no',
+];
 
 const shakeBall = () => {
 	ball.classList.add('shake-animation');
-	};
+	setTimeout(() => {
+		ball.classList.remove('shake-animation');
+	}, 1000);
+	showError();
+};
 
-const magicBall = () => {
-    shakeBall();
-    // ball.classList.remove('shake-animation');
-}
-
-ballImg.addEventListener('click', magicBall);
+ballImg.addEventListener('click', shakeBall);
